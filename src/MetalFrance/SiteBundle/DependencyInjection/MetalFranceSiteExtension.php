581b0d17b3ce016578dc939d4b1f51bdf853a582
Jérôme Vieilledent <lolautruche@gmail.com>
2013-09-10 15:28:08 +0200

    Delegate loading of mf.yml to main ezpublish.yml via import instead of MetalFranceSiteExtension to fix precedence issues

--- src/MetalFrance/SiteBundle/DependencyInjection/MetalFranceSiteExtension.php
+++ src/MetalFrance/SiteBundle/DependencyInjection/MetalFranceSiteExtension.php
@@ -24,6 +24,5 @@ class MetalFranceSiteExtension extends Extension
 
         $loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__.'/../Resources/config' ) );
         $loader->load( 'services.yml') ;
-        $loader->load( 'mf.yml' );
     }
 }
